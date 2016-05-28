2bd1278587800a3dc1c16e2a69b576a180413619
mc-crafty <mc_crafty@gmx.com>
2016-05-28 02:44:37 -0300

    fix deploy so it deploys new compressed js everytime. add translate page.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deploy goosh.org";
-echo "wget goosh.js";
+#echo "wget goosh.js";
 #wget http://gshell.grothkopp.com/goosh.js -O goosh.js.uncompr -o /dev/null
 #wget http://n.goosh.org/g.php -O goosh.js.uncompr -o /dev/null
 /g/OTHER/progTools/php/php g.php >goosh.js.uncompr
@@ -13,7 +13,15 @@ echo "gzip"
 
 cat goosh.js.compr.tmp |sed s\#gshell.grothkopp.com\#goosh.org\#g |sed s\#ABQIAAAA0cXSEVCNSwf_x74KTtPJMRShYK5vgJfK0afUKMRqjECszDItkhTOIyZ74499O_ys5nJIQuP4sq4nZg\#ABQIAAAA0cXSEVCNSwf_x74KTtPJMRQP4Q7D8MPck7bhT7upyfJTzVDU2BRxkUdd2AvzlDDF7DNUJI_Y4eB6Ug\#g >goosh.js.compr
 
-wget 'http://n.goosh.org/?deploy=1' -O goosh.html -o /dev/null
+wget 'http://n.goosh.org/?deploy=1' -O goosh.html-dl -o /dev/null
+
+scriptLine=`grep -n "var goosh=new Object();" goosh.html-dl | cut -d : -f 1`
+
+echo $scriptLine
+
+cat goosh.html-dl | head -n $(($scriptLine-1)) > goosh.html
+cat goosh.js.compr >> goosh.html
+cat goosh.html-dl | tail -n +$(($scriptLine+1)) >> goosh.html
 
 #exit
 echo "copy files"
